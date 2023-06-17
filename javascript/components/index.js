68c68c20144c3ade2794653275a5a9be9d467066
jos50275266 <jos50275266@gmail.com>
2023-06-17 18:38:18 +0900

    Part-4

--- javascript/components/index.js
+++ javascript/components/index.js
@@ -1,11 +1,11 @@
 const root = document.getElementById("root");
 const container = document.querySelector(".container");
 const pagination = document.querySelector(".pagination");
+let activeButton = "";
 
 async function run() {
   // "", false, 0, [], {}
-  let activeButton = "";
-  const { results, page, previous, count } = await getGames();
+  const { results, page, previous, count, next } = await getGames();
   //   const nextPageUrl = data.next;
   //   const numOfPages = data.count / 20;
   //   console.log(numOfPages);
@@ -13,49 +13,84 @@ async function run() {
   // https://api.rawg.io/api/games?key=15bb57f3ba884c3dabfd98e4ab984b28&page=
 
   visualizeData(results);
-
-  for (let page = 1; page < 10; page++) {
-    const button = document.createElement("button");
-    button.textContent = page;
-
-    if (page == 1) {
-      activeButton = button;
-      button.classList.add("active");
-    }
-
-    button.addEventListener("click", async (e) => {
-      e.preventDefault();
-
-      if (activeButton) {
-        activeButton.classList.remove("active");
-      }
-
-      activeButton = e.target;
-      activeButton.classList.add("active");
-
-      let data = await getGame(page);
-      visualizeData(data);
-    });
-
-    pagination.appendChild(button);
-  }
+  createNavigation(10);
 }
 
 run();
 
 function visualizeData(data) {
+  // page reset
   container.textContent = "";
 
   data.forEach((game) => {
-    const { name, rating, background_image, released } = game;
-    const div = document.createElement("div");
-    div.innerHTML = `<div>
-        <h1>Name: ${name}</h1>
-        <span>Rating: ${rating}</span>
-        <span>Released: ${released}</span>
-        <img style="width: 300px; height: 200px;" src=${background_image} />
-    </div>`;
-
-    container.appendChild(div);
+    const card = cardComponent(game);
+    container.appendChild(card);
   });
+}
+
+function cardComponent(cardInfo) {
+  // article, img, div > h3, p, a
+  // console.log("cardInfo: ", cardInfo);
+  // Destructing
+  const { name, rating, background_image, released } = cardInfo;
+  const cardWrapper = createElement("article", "", ["cardWrapper"]);
+  const cardImg = createElement("img", background_image, ["cardImg"]);
+  const cardContent = createElement("div", "");
+  const cardTitle = createElement("h3", name);
+  const cardRating = createElement("span", rating);
+  const cardReleased = createElement("span", released);
+
+  cardWrapper.appendChild(cardImg);
+  cardWrapper.appendChild(cardContent);
+
+  cardContent.appendChild(cardTitle);
+  cardContent.appendChild(cardRating);
+  cardContent.appendChild(cardReleased);
+
+  return cardWrapper;
+}
+
+function createElement(tag, text = "", classes = []) {
+  const element = document.createElement(tag);
+
+  classes.forEach((cls) => {
+    element.classList.add(cls);
+  });
+
+  if (tag === "img") {
+    element.setAttribute("src", text);
+  } else if (tag === "a") {
+    element.setAttribute("href", text);
+  } else {
+    element.textContent = text;
+  }
+
+  return element;
+}
+
+function createNavigation(numOfPages) {
+  for (let page = 1; page < numOfPages; page++) {
+    const pageBtn = document.createElement("button");
+    pageBtn.textContent = page;
+
+    if (page === 1) {
+      activeButton = pageBtn;
+      pageBtn.classList.add("active");
+    }
+
+    pageBtn.addEventListener("click", async (e) => {
+      e.preventDefault();
+
+      if (activeButton) {
+        activeButton.classList.remove("active");
+        activeButton = e.target;
+        activeButton.classList.add("active");
+
+        const newPageData = await getGame(page);
+        visualizeData(newPageData);
+      }
+    });
+
+    pagination.appendChild(pageBtn);
+  }
 }
